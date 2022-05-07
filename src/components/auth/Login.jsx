e2561f4c9e2e191fefe78eb3c9a0fb8cabfae256
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-05-07 18:59:34 -0300

    Estilos auth router

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,6 +1,20 @@
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <h3>Login</h3>
+    <form>
+      <input type="text" placeholder="email" name="email" />
+      <input type="password" placeholder="Password" name="password" />
+
+      <button type="submit"
+      
+      >Login</button>
+
+      <hr />
+      Google auth
+
+    </form>
+    </>
   )
 }
--- src/router/AuthRouter.jsx
+++ src/router/AuthRouter.jsx
@@ -4,11 +4,14 @@ import { Register } from "../components/auth/Register";
 
 export const AuthRouter = () => {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />}></Route>
-      <Route path="/register" element={<Register />}></Route>
-      <Route path="*" element={<Navigate to={'login'}/>}></Route>
-
-    </Routes>
+    <div className="auth__main">
+      <div className="auth__box-container">
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="*" element={<Navigate to={"login"} />}></Route>
+        </Routes>
+      </div>
+    </div>
   );
 };
