1d6a51b3a2bb5a5833b322d909edc951323bed99
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-05-07 22:08:34 -0300

    Note main content

--- src/components/notes/NoteScreen.jsx
+++ src/components/notes/NoteScreen.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import { NotesAppBar } from './NotesAppBar'
+import React from "react";
+import { NotesAppBar } from "./NotesAppBar";
 
 export const NoteScreen = () => {
   return (
-    <div className='notes__main-content'>
-        <NotesAppBar/>
+    <div className="notes__main-content">
+      <NotesAppBar />
+      <div className="notes__content">
+        <input
+          type="text"
+          placeholder="Some awesome title"
+          className="notes__title-input"
+          autoComplete="off"
+        />
+        <textarea
+          className="notes__textarea"
+          placeholder="What happen today?"
+        ></textarea>
+        <div className="notes__image">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5HfYHmqkZPU9ImjiYjUMIsK94eaDaQ364fg&usqp=CAU"
+            alt="imagen-note"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
