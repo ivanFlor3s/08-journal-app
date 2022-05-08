29458a413716e2f7a19e873f73b8d7d374748888
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-05-07 21:55:54 -0300

    NoteAppBar

--- src/components/journal/JournalScreen.jsx
+++ src/components/journal/JournalScreen.jsx
@@ -1,11 +1,14 @@
 import {Sidebar} from './Sidebar'
 // import {NothingSelected } from './NothingSelected'
+import { NoteScreen } from '../notes/NoteScreen'
 export const JournalScreen = () => {
   return (
     <div className='journal__main-content'>
       <Sidebar/>
       <main>
         {/* <NothingSelected></NothingSelected> */}
+
+        <NoteScreen></NoteScreen>
       </main>
     </div>
   )
