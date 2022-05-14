8214f0b4b412f8c0cd0f637cf8fd19f476ff56c0
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-05-14 14:36:06 -0300

    login del usuario con mail y password

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -3,9 +3,11 @@ import {firebase, googleAuthProvider} from '../firebase/firebase.config'
 
 export const startLoginEmailPassword = (email, password)=>{
     return (dispatch)=>{
-        setTimeout(() => {
-            dispatch(login(123,'pedro'))
-        }, 2000);
+        firebase.auth().signInWithEmailAndPassword(email,password)
+            .then(({user})=>{
+                dispatch(login(user.uid,user.displayName))
+             })
+             .catch(err => console.error(err))
     }
 }
 
@@ -29,6 +31,8 @@ export const startEmailRegister = (email, password, name)=>{
                 //Luego logueo al usuario registrado a la aplicacion
                 dispatch(login(user.uid, user.displayName))
             })
+            .catch(err => console.error(err))
+
     }
 }
 
