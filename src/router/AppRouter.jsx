a66a30a5c77f422d637949f6c5e8a063f90fc8ec
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-05-14 18:26:18 -0300

    mostrar espere antes de mostrar rutas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,5 @@
 
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { BrowserRouter as Browser , Routes, Route, Navigate } from "react-router-dom"
 import { JournalScreen } from "../components/journal/JournalScreen"
 import { AuthRouter } from "./AuthRouter"
@@ -12,17 +12,32 @@ export const AppRouter = () => {
 
   const dispatch = useDispatch()
 
+  //Checking true: Revisando estado de firebase
+  const [checking, setChecking] = useState(true)
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user)=>{
       if(user?.uid){
-        dispatch(login(user.uid, user.displayName))
+        dispatch(login(user.uid, user.displayName));
+        setIsLoggedIn(true)
       }
+      else {
+        setIsLoggedIn(false)
+      }
+      setChecking(false)
     })
   
     
-  }, [])
+  }, [setChecking,setIsLoggedIn])
   
 
+  if(checking){
+    return (
+      <h1>Espere...</h1>
+    )
+  }
   return (
     <Browser>
         <Routes>
