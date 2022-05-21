d5244b4b2a7fae73c22bb991d5e540189b2c539f
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-05-21 17:56:21 -0300

    Actualizar la nota actual

--- src/components/notes/NoteScreen.jsx
+++ src/components/notes/NoteScreen.jsx
@@ -1,27 +1,37 @@
 import React, { useEffect, useRef } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "../../hooks/useForm";
 import { NotesAppBar } from "./NotesAppBar";
+import { activeNote } from "../../actions/notes";
 
 export const NoteScreen = () => {
 
+  const dispatch = useDispatch()
+
   //Renombro active to note
   const {active: note} = useSelector(state=>state.notes)
-  console.log(note)
+
   const [ formValues, handleInputChange, reset] = useForm(note)
-  console.log(formValues)
+
   const { body, title} = formValues
 
   //uso useRef para tener el id de la nota
   //USE REF almaceno una variable mutable que no va a redibujar el componente si cambia
   const activeId = useRef(note.id)
+
   useEffect(()=>{
     //Solo disparar el reset cuando cambia el note.id
-    if(activeId !== note.id){
+    if(activeId.current !== note.id){
       reset(note)
       activeId.current = note.id
     }
   },[note, reset])
+
+  useEffect(() => {
+    dispatch(activeNote(formValues.id, {...formValues}))
+      
+  }, [dispatch, formValues])
+  
 
   return (
     <div className="notes__main-content">
@@ -32,12 +42,14 @@ export const NoteScreen = () => {
           placeholder="Some awesome title"
           className="notes__title-input"
           autoComplete="off"
+          name="title"
           value={title}
           onChange={handleInputChange}
         />
         <textarea
           className="notes__textarea"
           placeholder="What happen today?"
+          name="body"
           value={body}
           onChange={handleInputChange}
         ></textarea>
--- src/components/notes/NotesAppBar.jsx
+++ src/components/notes/NotesAppBar.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { startSaveNote } from '../../actions/notes'
 
 export const NotesAppBar = () => {
+
+  const {active} = useSelector(state => state.notes)
+  const dispatch = useDispatch()
+  const handleOnSave = ()=>{
+    dispatch(startSaveNote(active))
+  }
   return (
     <div className='notes__appbar'>
         <span>28 Agosto 2020</span>
         <div>
             <button className='btn'> Picture </button>
-            <button className='btn'> Save</button>
+            <button className='btn' onClick={handleOnSave}> Save</button>
         </div>
     </div>
   )
